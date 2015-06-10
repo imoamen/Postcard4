3c25bf2f4f68bbf312c20bfe98d0db88afebc659
Moamen <moamen@Moamens-Mac.local>
2015-06-09 21:25:03 -0700

    fourth ver

--- PostCard4/ViewController.swift
+++ PostCard4/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNametextfield: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func sendCongratsButtonPressed(sender: UIButton) {
+        nameLabel.hidden = false
+        nameLabel.text = enterNametextfield.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNametextfield.text = ""
+        enterNametextfield.resignFirstResponder()
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        
+        mailButton.setTitle("Congrats Sent", forState: UIControlState.Normal)
+    }
+    
 }
-
